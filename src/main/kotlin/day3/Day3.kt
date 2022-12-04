340a329cdfcb493052fa66e7c5f733373ec95d6c
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-12-04 10:02:54 +0100

    Day 3, part 2

--- src/main/kotlin/day3/Day3.kt
+++ src/main/kotlin/day3/Day3.kt
@@ -4,8 +4,8 @@ import day1.inputLines
 
 fun main() {
 	val result = inputLines()
-		.map { it.chunked(it.length / 2) }
-		.map { (first, second) -> first.first { it in second } }
+		.windowed(3, 3)
+		.map { (first, second, third) -> first.first { it in second && it in third } }
 		.map {
 			if (it in 'a'..'z') it.code - 'a'.code
 			else it.code - 'A'.code + 26
